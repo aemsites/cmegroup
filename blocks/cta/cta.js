6505823a2d560a759b5b44f46ab6fe0c24d030b6
Anuj Mahajan <anujmaha@adobe.com>
2025-02-11 23:01:01 +0530

    fix: promo cta and article page styling

---
+++ blocks/cta/cta.js
@@ -0,0 +1,37 @@
+import { createElement } from '../../scripts/utils.js';
+
+export default function decorate(block) {
+  if (block.classList.contains('promo')) {
+    const rows = block.querySelectorAll(':scope > div');
+    const linkWrapper = createElement('a', { href: '#' });
+    const contentWrapper = createElement('div', { class: 'content-wrapper' });
+    linkWrapper.append(contentWrapper);
+    for (const row of rows) {
+      const type = row.querySelector(':scope > div:first-child')?.textContent?.trim().toLowerCase();
+      const content = row.querySelector(':scope > div:last-child');
+
+      if (type === 'url') {
+        linkWrapper.href = content.textContent.trim();
+      } else {
+        if (type === 'background') {
+          content.classList.add('background-image');
+          linkWrapper.append(content);
+        } else {
+          if (type === 'title') {
+            content.classList.add('title');
+            contentWrapper.append(content);
+          } else if (type === 'description') {
+            content.classList.add('description');
+            contentWrapper.append(content);
+          } else if (type === 'footer') {
+            content.classList.add('footer');
+            linkWrapper.append(content);
+          }
+          
+        }
+      }
+    }
+    block.innerHTML = '';
+    block.appendChild(linkWrapper);
+  }
+}
