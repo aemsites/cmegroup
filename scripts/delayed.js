847dbb05b0e519d8ec8f44b2c9df4e17570f4ea7
Nestor de Dios <>
2025-02-18 12:41:37 -0300

    Issue 56: Education landing page - Comments from PR

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -2,7 +2,6 @@ import { loadScript } from './aem.js';
 
 // add delayed functionality here
 function loadShareThis() {
-  // Sharethis
   loadScript('https://platform-api.sharethis.com/js/sharethis.js#property=644646a57ac381001a304496&product=sticky-share-buttons&source=platform');
 }
 
