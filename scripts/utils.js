fe30a087d2c1f611a2dc79acae87e2fe82761d89
Ramiro DiModica <rdimodica@majorkeytech.com>
2025-02-27 12:12:47 +0100

    issue-15-embed-brightcove video block

--- scripts/utils.js
+++ scripts/utils.js
@@ -79,9 +79,30 @@ function parseTime(time) {
   return `${timeInMins} min`;
 }
 
+async function loadScript(src, attrs) {
+  return new Promise((resolve, reject) => {
+    if (!document.querySelector(`head > script[src="${src}"]`)) {
+      const script = document.createElement('script');
+      script.src = src;
+      if (attrs) {
+      // eslint-disable-next-line no-restricted-syntax, guard-for-in
+        for (const attr in attrs) {
+          script.setAttribute(attr, attrs[attr]);
+        }
+      }
+      script.onload = resolve;
+      script.onerror = reject;
+      document.head.append(script);
+    } else {
+      resolve();
+    }
+  });
+}
+
 export {
   createElement,
   getArticleRelatedMetadata,
   addDividerLine,
   parseTime,
+  loadScript,
 };
