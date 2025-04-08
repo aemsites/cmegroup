b113167b8afdbc862320f92f18956d9c5a25bce5
Joaquin Allende <jallende@majorkeytech.com>
2025-04-08 11:20:10 -0300

    Issue 105: updated env endpoint alert

--- aemedge/scripts/alerts/alerts.js
+++ aemedge/scripts/alerts/alerts.js
@@ -3,12 +3,12 @@
  */
 
 import { getMetadata } from '../aem.js';
-import { getBrowserName } from '../utils.js';
+import { getBrowserName, getEnvType } from '../utils.js';
 
 const CACHE_KEY = 'alerts_cache';
 const CACHE_TIMESTAMP_KEY = 'alerts_cache_timestamp';
 const CACHE_DURATION = 15 * 60 * 1000;
-const alertsEndpoint = 'https://beta.cmegroup.com/content/cmegroup/en/misc/api/content-feeds-for-google-docs/full-alerts-list/jcr:content/main-content-section/section/section-elements/search_sort_filter_d.ssfajax.0.json';
+const alertsEndpoint = `${getEnvType() !== 'prod' ? 'https://beta.cmegroup.com' : 'https://www.cmegroup.com'}/content/cmegroup/en/misc/api/content-feeds-for-google-docs/full-alerts-list/jcr:content/main-content-section/section/section-elements/search_sort_filter_d.ssfajax.0.json`;
 let alertsPromise = null;
 
 function decodeHTML(html) {
