cf67523eb6126c34ca81e3e4a81dfef0b5388680
ramirodimodica1 <49731792+ramirodimodica1@users.noreply.github.com>
2025-04-23 11:01:01 +0200

    CUEN-61 close navbar after click on item (#183)
    Co-authored-by: Ramiro DiModica <rdimodica@majorkeytech.com>

--- aemedge/blocks/header/header.js
+++ aemedge/blocks/header/header.js
@@ -639,6 +639,14 @@ class Nav {
       e.stopPropagation();
       this.toggleMenu(subNavItem);
     });
+
+    const linksInNav = subMenu.querySelectorAll('li a');
+    linksInNav.forEach((link) => {
+      link.addEventListener('click', () => {
+        this.closeNav(this.nav);
+      });
+    });
+
     return subMenu;
   };
 
