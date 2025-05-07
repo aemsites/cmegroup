4cd834cc5f107d858e908613bc28bdb51e03d9ea
matiasgomezcarabias <matias.gomezcarabias@cmegroup.com>
2025-05-07 17:26:58 -0300

    Hardcoding referer to avoid cors

--- aemedge/blocks/cards/cards.js
+++ aemedge/blocks/cards/cards.js
@@ -314,8 +314,6 @@ async function fetchAndFilterUpcomingEvent() {
       headers: {
         Accept: 'application/json',
         'Content-Type': 'application/json',
-        referer: 'https://beta.cmegroup.com/education_qa',
-        Referer: 'https://beta.cmegroup.com/education_qa',
       },
       body: JSON.stringify({
         date: getCurrentDateFormatted(),
