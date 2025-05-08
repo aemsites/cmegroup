550b73a291918c5691d21cf6f7565fe0204dfeb7
Ramiro DiModica <rdimodica@majorkeytech.com>
2025-05-08 18:29:46 +0200

    add isProtected=1 param in api get/post - CORS fix

--- aemedge/scripts/utils/fetch.js
+++ aemedge/scripts/utils/fetch.js
@@ -1,49 +1,44 @@
 import { isEmpty, axiosGet, axiosPost } from './misc.js';
 
-// function makeProtectedUrl(url, withCache = false) {
-//   if (window.forceUseCache || withCache) {
-//     return url;
-//   }
-//   return `${url}${
-//     /\?/.test(url) ? '&' : '?'
-//   }isProtected&_t=${new Date().getTime()}`;
-// }
+function makeProtectedUrl(url, withCache = false) {
+  if (window.forceUseCache || withCache) {
+    return url;
+  }
+  return `${url}${
+    /\?/.test(url) ? '&' : '?'
+  }isProtected=1&_t=${new Date().getTime()}`;
+}
 
 export function apiGet(
   url,
   params = {},
   headers = {},
-  // withCache = false,
+  withCache = false,
 ) {
   const baseUrl = window.baseUrl || '';
   const data = { params, headers };
-  // return axiosGet(baseUrl + makeProtectedUrl(url, withCache), data);
-  return axiosGet(baseUrl + url, data);
+  return axiosGet(baseUrl + makeProtectedUrl(url, withCache), data);
 }
 
 export function apiGetAbsolute(
   url,
   params = {},
   headers = {},
-  // withCache = false,
+  withCache = false,
 ) {
   const baseUrl = window.baseUrl || '';
   const data = { params, headers };
-  // return axiosGet(baseUrl + makeProtectedUrl(url, withCache), true, data);
-  return axiosGet(baseUrl + url, true, data);
+  return axiosGet(baseUrl + makeProtectedUrl(url, withCache), true, data);
 }
 
 export function apiPost(
   url,
   params = {},
   headers = {},
-  // withCache = false,
+  withCache = false,
 ) {
   const baseUrl = window.baseUrl || '';
-  // return axiosPost(baseUrl + makeProtectedUrl(url, withCache), params, {
-  //   headers,
-  // });
-  return axiosPost(baseUrl + url, params, {
+  return axiosPost(baseUrl + makeProtectedUrl(url, withCache), params, {
     headers,
   });
 }
@@ -52,13 +47,10 @@ export function apiPostAbsolute(
   url,
   params = {},
   headers = {},
-  // withCache = false,
+  withCache = false,
 ) {
   const baseUrl = window.baseUrl || '';
-  // return axiosPost(baseUrl + makeProtectedUrl(url, withCache), params, true, {
-  //   headers,
-  // });
-  return axiosPost(baseUrl + url, params, true, {
+  return axiosPost(baseUrl + makeProtectedUrl(url, withCache), params, true, {
     headers,
   });
 }
