5ab268d66acde5a9190af15f82a3ddf29f4bc424
Nestor de Dios <>
2025-05-14 14:24:59 -0300

    Issue 238: Tags exclusions on tags cloud block

--- aemedge/blocks/tags-cloud/tags-cloud.js
+++ aemedge/blocks/tags-cloud/tags-cloud.js
@@ -1,14 +1,54 @@
 import { readBlockConfig } from '../../scripts/aem.js';
 import { i18n, getPageTags, createElement } from '../../scripts/utils.js';
+import ffetch from '../../scripts/ffetch.js';
 
 const defaultUrl = '/education/browse-all';
 
-function buildTagList(block, tagsLablel, tags, listPage) {
-  if (!tags) return;
+/**
+ * Tags Cloud Exclusions
+ */
+const tagsCloudExclusionsEndpoint = '/eds-config/tags-cloud-exclusions.json';
+let tagsCloudExclusionsPromise = null;
+
+function fetchTagsCloudExclusions() {
+  if (!tagsCloudExclusionsPromise) {
+    tagsCloudExclusionsPromise = new Promise((resolve, reject) => {
+      (async () => {
+        try {
+          const tagsCloudExclusionsJson = await ffetch(`${tagsCloudExclusionsEndpoint}`).all();
+          const tagsCloudExclusions = [];
+          tagsCloudExclusionsJson.forEach((row) => {
+            tagsCloudExclusions.push(row.tag);
+          });
+          resolve(tagsCloudExclusions);
+        } catch (e) {
+          reject(e);
+        }
+      })();
+    });
+  }
+  return tagsCloudExclusionsPromise;
+}
+
+async function filterIncludedTags(tags) {
+  const exclusions = await fetchTagsCloudExclusions();
+  return tags.filter((tag) => !exclusions.some((exclusion) => (
+    exclusion.endsWith('/')
+      ? tag.name.startsWith(exclusion)
+      : tag.name === exclusion)));
+}
+
+/**
+ * Build tags list
+ */
+async function buildTagList(block, tagsLablel, tags, listPage) {
+  if (!tags || tags.length === 0) return;
+  const filteredTags = await filterIncludedTags(tags);
+  if (!filteredTags || filteredTags.length === 0) return;
   const titleSpan = createElement('span', { class: 'tag-label' });
   titleSpan.textContent = `${tagsLablel}:`;
   block.append(titleSpan);
-  tags.forEach((tag) => {
+  filteredTags.forEach(async (tag) => {
     const { name, title } = tag;
     const button = createElement('a', {
       class: 'btn-tag-filter',
