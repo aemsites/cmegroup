e977d069bf6f9af13130fb652c8648e0cb42e0b1
Piyush Jindal <jindaliiita@gmail.com>
2025-05-21 09:56:47 +0530

    Setting cache time 0 for courses/lessons (#271)
    * Setting cache time 0 for courses/lessons
    
    * Comment added

--- aemedge/scripts/course/course.js
+++ aemedge/scripts/course/course.js
@@ -12,8 +12,9 @@ const LESSONS_BASE_PATH = '/education/lessons/';
 const COURSES_INDEX_PATH = '/courses-index.json';
 const TEMPLATES = ['course', 'chapter', 'lesson', 'lesson-standalone'];
 const CACHE_KEY = 'course_data';
-const CACHE_EXPIRATION_PROD = 15 * 60 * 1000; // 15 minutes in milliseconds
-const CACHE_EXPIRATION_STAGE = 30 * 1000; // 30 seconds in milliseconds
+// TODO: Anuj, we need to review this cache timing again.
+const CACHE_EXPIRATION_PROD = 0; // 15 * 60 * 1000; // 15 minutes in milliseconds
+const CACHE_EXPIRATION_STAGE = 0; // 30 * 1000; // 30 seconds in milliseconds
 
 const isLessonStandalone = (template) => template.toLowerCase() === 'lesson-standalone';
 const getQueryIndexUrl = () => {
