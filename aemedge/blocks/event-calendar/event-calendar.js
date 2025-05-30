0db5a2519926830a69537020f90cd6ac3253b854
Ramiro DiModica <rdimodica@majorkeytech.com>
2025-05-30 12:46:56 +0200

    calendar-table-fixs

--- aemedge/blocks/event-calendar/event-calendar.js
+++ aemedge/blocks/event-calendar/event-calendar.js
@@ -5,7 +5,7 @@ import {
   postEconomicReleaseDates,
   postEconomicReleaseEvents,
 } from '../../scripts/services/ProductCalendarService.js';
-import { URIUtil } from '../../scripts/utils/index.js';
+import { URIUtil, escapeHtmlTags } from '../../scripts/utils/index.js';
 
 const uriUtil = new URIUtil('', URIUtil.ARRAY_COMMA_ENCODE);
 const { body } = window.document;
@@ -273,12 +273,11 @@ function decorateCleanInputSearch() {
   cleanInput.addEventListener('click', async () => {
     cleanInputSearch();
   });
-  return cleanInput;
 }
 
 function handleInputSearch(e) {
   searchValueVar = e.target.value;
-  filterSearchInputContainer.append(decorateCleanInputSearch());
+  filterSearchInputContainer.append(cleanInput);
   if (searchValueVar !== '' && window.innerWidth >= 1200) {
     clearTimeout(timeoutId);
     timeoutId = setTimeout(() => {
@@ -585,6 +584,7 @@ function renderInputs() {
   const inputsFlexContainer = createElement('div', { class: 'inputs-flex-container' });
   filterSearchInput.placeholder = searchByEventLabel;
   filterSearchInputContainer.append(filterSearchInput);
+  decorateCleanInputSearch();
   inputsFlexContainer.append(filterSearchInputContainer);
 
   if (economicFilters && economicFilters.countries) {
@@ -939,7 +939,10 @@ function renderDesktopAccordion(eventsToRender) {
               <span class="highlight">${title}</span>
               <div class="main-content">
                 <div class="left-section">
-                  <p class="text">${text}</p>
+                  <p class="text">${escapeHtmlTags(text, ['br'])?.replace(
+      /<br\s*\/?>\s*(<br\s*\/?>)+/g,
+      '<br>',
+    )}</p>
                   <div class="more">
                     <span class="icon"></span>
                     <a href=${url}>Read more</a>
@@ -1041,7 +1044,10 @@ function renderMobileAccordion(eventsToRender) {
             <div class="expandable-content mobile">
               <div class="main-content">
                 <span class="highlight">${title}</span>
-                <p class="text">${text}</p>
+                <p class="text"><p class="text">${escapeHtmlTags(text, ['br'])?.replace(
+      /<br\s*\/?>\s*(<br\s*\/?>)+/g,
+      '<br>',
+    )}</p>
                 <div class="tags-section">
                   <span class="bold">Tags:</span>
                   <div>
