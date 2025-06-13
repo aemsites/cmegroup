a971e84ec80f4d9a654be1a8000b23b71fda8893
nawaz-adobe <115994423+nawaz-adobe@users.noreply.github.com>
2025-06-13 14:51:36 +0530

    fix: use one trust SSR (#337)
    * fix: use one trust SSR
    
    * fix: load one trust in delayed mode
    
    * fix: code refactor
    
    * fix: performance issue
    
    * fix: lint issue
    
    * fix: try lazy loading
    
    * fix: revert lazy loading
    
    * fix: load one trust via GTM
    
    ---------
    
    Co-authored-by: Kunwar Saluja <saluja@adobe.com>

--- aemedge/scripts/delayed.js
+++ aemedge/scripts/delayed.js
@@ -1,5 +1,4 @@
 import { sampleRUM, loadScript } from './aem.js';
-import { getEnvType } from './utils.js';
 import loadSitewidePopups from './popups/popups.js';
 
 // Core Web Vitals RUM collection
@@ -9,47 +8,9 @@ function loadShareThis() {
   loadScript('https://platform-api.sharethis.com/js/sharethis.js#property=644646a57ac381001a304496&product=sticky-share-buttons&source=platform');
 }
 
-async function loadOneTrust() {
-  if (getEnvType() !== 'prod') {
-    return Promise.resolve();
-  }
-
-  const ONETRUST_CONFIG = {
-    stubScript: {
-      url: 'https://cdn.cookielaw.org/scripttemplates/otSDKStub.js',
-      options: {
-        type: 'text/javascript',
-        charset: 'UTF-8',
-      },
-    },
-    mainScript: {
-      url: 'https://cdn.cookielaw.org/consent/f42915b0-68e5-491a-a7f7-1db0d962ddff/OtAutoBlock.js',
-      options: {
-        type: 'text/javascript',
-        charset: 'UTF-8',
-        'data-domain-script': 'f42915b0-68e5-491a-a7f7-1db0d962ddff',
-      },
-    },
-  };
-
-  try {
-    await loadScript(ONETRUST_CONFIG.stubScript.url, ONETRUST_CONFIG.stubScript.options);
-    await loadScript(ONETRUST_CONFIG.mainScript.url, ONETRUST_CONFIG.mainScript.options);
-    if (!window.OneTrust) {
-      throw new Error('OneTrust failed to initialize');
-    }
-    return Promise.resolve();
-  } catch (error) {
-    // eslint-disable-next-line no-console
-    console.error('OneTrust loading failed:', error);
-    return Promise.resolve();
-  }
-}
-
 function loadPage() {
   loadSitewidePopups();
   loadShareThis();
-  loadOneTrust();
 }
 
 loadPage();
