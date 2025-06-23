bef49780f1e7a4a67ff7ee3fa778a326a2dab3af
Ramiro DiModica <rdimodica@majorkeytech.com>
2025-06-23 12:39:57 -0300

    issue-393-video-lcp preload auto

--- aemedge/blocks/brightcove/brightcove.js
+++ aemedge/blocks/brightcove/brightcove.js
@@ -282,7 +282,8 @@ export default async function decorate(block) {
             ${playlistId !== '' && videoId ? `data-playlist-video-id="${videoId}"` : ''}
             ${playlistId === '' ? `data-video-id="${videoId}"` : ''}
             data-application-id="true"
-            preload="none">
+            preload="none"
+            loading="lazy">
           </video-js>
           ${playlistId !== '' && playlistLocation === 'R' ? '<div class="vjs-playlist"></div>' : ''}
         </div>
