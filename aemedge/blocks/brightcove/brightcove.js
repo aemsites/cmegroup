85e6eedc0034e1c9b9074baea48c78f3068a2217
Ramiro DiModica <rdimodica@majorkeytech.com>
2025-07-03 11:38:15 +0200

    brightcove video aspect ratio

--- aemedge/blocks/brightcove/brightcove.js
+++ aemedge/blocks/brightcove/brightcove.js
@@ -1,5 +1,10 @@
 import { readBlockConfig, getMetadata } from '../../scripts/aem.js';
-import { setTracking, apiGetAbsolute, LocalStorageUtil, getRandomNumber } from '../../scripts/utils/index.js';
+import {
+  setTracking,
+  apiGetAbsolute,
+  LocalStorageUtil,
+  getRandomNumber,
+} from '../../scripts/utils/index.js';
 /*
  * For more info about the video's options please read:
  * https://github.com/brightcove/player-loader
