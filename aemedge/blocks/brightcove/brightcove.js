64e0c1dd74cc2b3d2853601eabfbff3edb5ea097
Ramiro DiModica <rdimodica@majorkeytech.com>
2025-07-04 13:37:11 +0200

    video placeholder click

--- aemedge/blocks/brightcove/brightcove.js
+++ aemedge/blocks/brightcove/brightcove.js
@@ -348,12 +348,7 @@ export default async function decorate(block) {
     <div
       class="brightcove-img-placeholder ${playlistId !== '' && playlistLocation === 'R' ? 'playlist-right' : ''}"
       style="background-image: url('${videoPoster}')">
-        // <div class="lds-ring spinner-in-video">
-        //   <div></div>
-        //   <div></div>
-        //   <div></div>
-        //   <div></div>
-        // </div>
+        <div class="placeholder-play-btn"></div>
     </div>
     <div class='brightcove-video'>
       <div class='brightcove-wrapper'>
