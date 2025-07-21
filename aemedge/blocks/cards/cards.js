0be4dfa1c9dfd3d2d277b063c1e45ea230b6b743
nawaz-adobe <115994423+nawaz-adobe@users.noreply.github.com>
2025-07-21 18:23:13 +0530

    fix: related courses style issue (#440)
    * fix: import
    
    * fix: css loading
    
    * fix: build issue
    
    * fix: race condition
    
    * fix: lint errors
    
    * fix: refactor code
    
    * fix: adjust code
    
    * fix: css changes
    
    * fix: use nested css
    
    ---------
    
    Co-authored-by: Kunwar Saluja <saluja@adobe.com>

--- aemedge/blocks/cards/cards.js
+++ aemedge/blocks/cards/cards.js
@@ -1,10 +1,9 @@
 /* eslint-disable max-len */
-import createOptimizedPicture from '../../scripts/utils/picture.js';
 import {
   buildIndexFilter,
   getIndexedContent,
 } from '../../scripts/indexing.js';
-import { getEconomicReleaseEvents } from '../../scripts/services/EconomicReleaseService.js';
+
 import {
   createElement,
   parseTime,
@@ -16,6 +15,9 @@ import {
   getUTCfromDateString,
   readBlockConfig,
 } from '../../scripts/utils.js';
+
+import createOptimizedPicture from '../../scripts/utils/picture.js';
+import { getEconomicReleaseEvents } from '../../scripts/services/EconomicReleaseService.js';
 
 async function createStaticCards(block) {
   const cardsContainer = document.createElement('div');
@@ -285,7 +287,7 @@ function createSpinner() {
   return spinner;
 }
 
-export async function createDynamicCards(block, numEntries = null) {
+export async function createDynamicCards(block) {
   const config = readBlockConfig(block);
   block.textContent = '';
   block.append(createSpinner());
@@ -297,11 +299,8 @@ export async function createDynamicCards(block, numEntries = null) {
   if (block.classList.contains('course')) {
     const indexFilter = buildIndexFilter(config);
     indexFilter.templates = ['course'];
-    indexFilter.orderBy = 'date';
+    indexFilter.orderBy = 'lastModified';
     indexFilter.sortDirection = 'desc';
-    if (numEntries) {
-      indexFilter.limit = numEntries;
-    }
     filteredData = await getIndexedContent(indexFilter);
     sliderConfig = {
       slidesToShow: 'auto',
