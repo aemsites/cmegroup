851b341a748236da740a4f6fb530d7c1a5a52981
Ramiro DiModica <rdimodica@majorkeytech.com>
2025-07-24 17:52:36 +0200

    new card variation static in slider

--- aemedge/blocks/cards/cards.js
+++ aemedge/blocks/cards/cards.js
@@ -90,62 +90,10 @@ async function createStaticCards(block) {
     }
     cardsContainer.append(mainContainer);
   } else if (block.classList.contains('static')) {
-    const [
-      readLabel,
-      watchLabel,
-    ] = await Promise.all([
-      i18n('Read'),
-      i18n('Watch'),
-    ]);
-    const ul = document.createElement('ul');
-    [...block.children].forEach((row) => {
-      const li = document.createElement('li');
-      const image = row.querySelector('picture');
-      const title = row.querySelector('h3').innerText;
-      const linkSrc = row.querySelector('h3 a').href;
-      const date = row.querySelector('strong').innerText;
-      const format = row.querySelector('em').innerText;
-      const time = row.querySelector('em').parentNode.parentNode.nextElementSibling.querySelector('p').innerText;
-
-      const linkEl = document.createElement('a');
-      linkEl.href = linkSrc;
-      linkEl.classList.add(`${format === 'video' ? 'video-card' : 'article-card'}`);
-
-      const imageContainer = document.createElement('div');
-      imageContainer.className = 'cards-image-container';
-      imageContainer.append(image);
-      imageContainer.querySelectorAll('picture > img').forEach((img) => img.closest('picture').replaceWith(createOptimizedPicture(img.src, img.alt, false, [{ width: '750' }])));
-
-      const mainContainer = document.createElement('div');
-      mainContainer.className = 'cards-body-container';
-      const cardSubtitle = document.createElement('div');
-      cardSubtitle.className = 'cards-subtitle';
-      const cardTime = document.createElement('span');
-      cardTime.className = 'cards-time';
-      cardTime.innerText = `${parseTime(time)} ${format === 'video' ? watchLabel : readLabel}`;
-      const cardDate = document.createElement('span');
-      cardDate.className = 'cards-date';
-      cardDate.innerText = date;
-      const cardTitle = document.createElement('h3');
-      cardTitle.innerHTML = title;
-
-      mainContainer.append(cardTime);
-      mainContainer.append(cardDate);
-      mainContainer.append(cardTitle);
-
-      linkEl.append(imageContainer);
-      linkEl.append(mainContainer);
-
-      li.append(linkEl);
-      ul.append(li);
-    });
-
-    cardsContainer.append(ul);
-  } else if (block.classList.contains('static-slider')) {
     const cardElements = [];
     let sliderConfig = null;
     let disabledOnDesktop = false;
-    let inverse = false;
+    const inverse = false;
 
     [...block.children].forEach((row) => {
       const li = createElement('li');
@@ -164,7 +112,15 @@ async function createStaticCards(block) {
 
       cardTitleContainer.append(courseQty);
       cardTitleContainer.append(title);
-      cardTextContainer.append(text);
+
+      if (
+        text
+        && text.tagName.toLowerCase() === 'p'
+        && !text.classList.contains('button-container')
+      ) {
+        cardTextContainer.append(text);
+      }
+
       cardBody.append(cardTitleContainer);
       cardBody.append(cardTextContainer);
       cardBody.append(linkEl);
@@ -193,8 +149,7 @@ async function createStaticCards(block) {
         },
       ],
     };
-    inverse = true;
-    disabledOnDesktop = true;
+    disabledOnDesktop = cardElements.length <= 5;
 
     if (cardElements && cardElements.length) {
       const ul = createElement('ul', null, ...cardElements);
