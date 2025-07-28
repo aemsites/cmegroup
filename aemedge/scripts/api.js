c6025257b46c0476c936f9551e3d2d8db582147a
Mariano Jurado <50640219+mjurado82@users.noreply.github.com>
2025-07-28 13:23:18 -0300

    Issue 488: Ecucation landing page 'Contact Us form' - logged in user (#489)

--- aemedge/scripts/api.js
+++ aemedge/scripts/api.js
@@ -33,24 +33,6 @@ async function getRecaptchaToken(form, { config }) {
   return recaptcha.getToken();
 }
 
-async function getUserInfo() {
-  return new Promise((resolve) => {
-    resolve({
-      First_Name__c: 'John',
-      Last_Name__c: 'Doe',
-      Email__c: 'john.doe@example.com',
-      Country_Code__c: 'US',
-      Job_Role__c: 'Technology',
-      Company_Type__c: 'FinTech',
-      Company_Name__c: 'Example Inc',
-      Message__c: 'This is a test message',
-      Privacy_Policy__c: true,
-      Terms_of_Service__c: true,
-      Receive_Communications__c: true,
-    });
-  });
-}
-
 async function fetchWithErrorHandling(url, method, options) {
   const { headers, body, payload } = options;
   try {
@@ -179,7 +161,6 @@ async function postForm(form, options = {}) {
 }
 
 export {
-  getUserInfo,
   postForm,
   fetchWithErrorHandling,
 };
