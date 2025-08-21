2824202f0a09915a052c6240cc3309fcef96922b
Nestor de Dios <>
2025-08-21 10:13:39 -0300

    Issue-607: Update cards block to remove legacy endpoint

--- aemedge/scripts/legacyContentMapping.js
+++ aemedge/scripts/legacyContentMapping.js
@@ -29,7 +29,68 @@ function convertMediaTypeToSubtemplate(mediaType) {
   }
 }
 
+function normalizeLegacyPath(path) {
+  let newPath = path;
+  if (newPath.startsWith('/content/cmegroup/en')) {
+    newPath = newPath.slice('/content/cmegroup/en'.length);
+  } else if (newPath.startsWith('/content/cmegroup')) {
+    newPath = newPath.slice('/content/cmegroup'.length);
+  }
+  if (!newPath.endsWith('.html')) {
+    newPath += '.html';
+  }
+  return newPath;
+}
+
+function mapLegacyArticleData(legacyData) {
+  const {
+    metadata: {
+      thumbnailImage: image,
+      primaryTopics,
+    },
+    title,
+    description,
+    path,
+    readTime,
+    date,
+    mediaType,
+  } = legacyData;
+  const durationMin = convertReadTimeFormat(readTime);
+  const subTemplates = convertMediaTypeToSubtemplate(mediaType);
+  const newPath = normalizeLegacyPath(path);
+  return {
+    path: newPath,
+    date,
+    title,
+    description,
+    readTime: durationMin,
+    metadata: {
+      'sub-template': subTemplates,
+      image,
+      'primary-topic': primaryTopics,
+    },
+  };
+}
+
+function isLegacyArticle(content) {
+  return content.source === 'onprem';
+}
+
+const legacyArticleTemplates = [
+  '/apps/cmegroup/templates/articleContentTemplate',
+  '/conf/cmegroupaem/settings/wcm/templates/cme-group-case-study-article-template',
+  '/conf/cmegroupaem/settings/wcm/templates/cme-group-faqs-article-template',
+  '/conf/cmegroupaem/settings/wcm/templates/cme-group-media-mentions-article-template',
+  '/conf/cmegroupaem/settings/wcm/templates/cme-group-newsletter-article-template',
+  '/conf/cmegroupaem/settings/wcm/templates/cme-group-podcast-article-template',
+  '/conf/cmegroupaem/settings/wcm/templates/cme-group-showcase-article-template',
+  '/conf/cmegroupaem/settings/wcm/templates/cme-group-standard-article-template',
+  '/conf/cmegroupaem/settings/wcm/templates/cme-group-video-article-template',
+  '/conf/cmegroupaem/settings/wcm/templates/cme-group-brightcove-video-template',
+];
+
 export {
-  convertReadTimeFormat,
-  convertMediaTypeToSubtemplate,
+  legacyArticleTemplates,
+  mapLegacyArticleData,
+  isLegacyArticle,
 };
