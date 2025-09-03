5d2e743da059143dc3c403e2200054a96ef410a3
Germán <2252731+gmaye@users.noreply.github.com>
2025-09-03 13:21:10 -0300

    feat(cuwst-439): login flow update (#660)
    * feat(cuwst-439): login flow update
    
    * feat(cuwst-439): cookie util setter update
    
    * fix(cuwst-439): cookie util lint issue
    
    ---------
    
    Co-authored-by: Germán Maye - E23970 <german.maye@cmegroup.com>

--- aemedge/scripts/legacy-api.js
+++ aemedge/scripts/legacy-api.js
@@ -4,10 +4,6 @@ export function getLegacyAlerts() {
   return `${urlByEnvType()}/content/cmegroup/en/misc/api/content-feeds-for-google-docs/full-alerts-list/jcr:content/main-content-section/section/section-elements/search_sort_filter_d.ssfajax.0.json`;
 }
 
-export function getLoginDataUrl(fromUrl, fromUrlTitle) {
-  return `${urlByEnvType()}/libs/cmegroup/security/login?fromUrl=${fromUrl}&fromUrlTitle=${fromUrlTitle}`;
-}
-
 export function getSearchSuggestionsUrl(term) {
   return `${urlByEnvType()}/bin/service/search.${term}.json`;
 }
