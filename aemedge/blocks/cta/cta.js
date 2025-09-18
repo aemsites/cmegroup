ada5331cf51b3efd4c70b93ebe7fb3abf7e15ffb
Ramiro DiModica <rdimodica@majorkeytech.com>
2025-09-18 15:29:47 +0200

    landing pages fixes

--- aemedge/blocks/cta/cta.js
+++ aemedge/blocks/cta/cta.js
@@ -37,4 +37,42 @@ export default function decorate(block) {
   if (block.classList.contains('divider-line')) {
     addDividerLine(block);
   }
+
+  if (block.classList.contains('promo-two-columns')) {
+    const rows = block.querySelectorAll(':scope > div');
+    const linkWrapper = createElement('a', { href: '#' });
+    const promoWrapper = createElement('div', { class: 'promo-wrapper' });
+    const mainWrapper = createElement('div', { class: 'content-wrapper' });
+    const leftContentWrapper = createElement('div', { class: 'content-wrapper-left' });
+    const rightContentWrapper = createElement('div', { class: 'content-wrapper-right' });
+    Array.from(rows).forEach((row) => {
+      const type = row.querySelector(':scope > div:first-child')?.textContent?.trim().toLowerCase();
+      const content = row.querySelector(':scope > div:last-child');
+
+      if (type === 'url') {
+        linkWrapper.href = content.textContent.trim();
+      } else if (type === 'background') {
+        content.classList.add('background-image');
+        promoWrapper.append(content);
+      } else if (type === 'eyebrow') {
+        content.classList.add('eyebrow');
+        leftContentWrapper.append(content);
+      } else if (type === 'title') {
+        content.classList.add('title');
+        leftContentWrapper.append(content);
+      } else if (type === 'description') {
+        content.classList.add('description');
+        rightContentWrapper.append(content);
+      } else if (type === 'footer') {
+        linkWrapper.innerHTML = content.innerHTML;
+      }
+    });
+
+    block.innerHTML = '';
+    rightContentWrapper.append(linkWrapper);
+    mainWrapper.append(leftContentWrapper);
+    mainWrapper.append(rightContentWrapper);
+    promoWrapper.append(mainWrapper);
+    block.appendChild(promoWrapper);
+  }
 }
