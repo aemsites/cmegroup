0540227d23493706a3f86515d1181efb381612de
Nestor de Dios <>
2025-10-01 11:53:12 -0300

    shovel raw html POC

--- aemedge/blocks/fragment/fragment.js
+++ aemedge/blocks/fragment/fragment.js
@@ -11,6 +11,8 @@ import {
 
 import {
   loadSections,
+  loadCSS,
+  loadScript,
 } from '../../scripts/aem.js';
 
 /**
@@ -19,30 +21,28 @@ import {
  * @returns {HTMLElement} The root element of the fragment
  */
 export async function loadFragment(path) {
-  if (path && path.startsWith('/')) {
-    const resp = await fetch(`${path}.plain.html`);
-    if (resp.ok) {
-      const main = document.createElement('main');
-      main.innerHTML = await resp.text();
+  const resp = await fetch(`${path}${path.endsWith('.html') ? '' : '.plain.html'}`);
+  if (resp.ok) {
+    const main = document.createElement('main');
+    main.innerHTML = await resp.text();
 
-      // reset base path for media to fragment base
-      const resetAttributeBase = (tag, attr) => {
-        main.querySelectorAll(`${tag}[${attr}^="./media_"]`).forEach((elem) => {
-          try {
-            elem[attr] = new URL(elem.getAttribute(attr), new URL(path, window.location)).href;
-          } catch (error) {
-            // Fallback if URL construction fails (eg. in sidekick shadow root)
-            elem[attr] = `${path}/${elem.getAttribute(attr).replace('./', '')}`;
-          }
-        });
-      };
-      resetAttributeBase('img', 'src');
-      resetAttributeBase('source', 'srcset');
+    // reset base path for media to fragment base
+    const resetAttributeBase = (tag, attr) => {
+      main.querySelectorAll(`${tag}[${attr}^="./media_"]`).forEach((elem) => {
+        try {
+          elem[attr] = new URL(elem.getAttribute(attr), new URL(path, window.location)).href;
+        } catch (error) {
+          // Fallback if URL construction fails (eg. in sidekick shadow root)
+          elem[attr] = `${path}/${elem.getAttribute(attr).replace('./', '')}`;
+        }
+      });
+    };
+    resetAttributeBase('img', 'src');
+    resetAttributeBase('source', 'srcset');
 
-      decorateMain(main);
-      await loadSections(main);
-      return main;
-    }
+    decorateMain(main);
+    await loadSections(main);
+    return main;
   }
   return null;
 }
@@ -50,12 +50,22 @@ export async function loadFragment(path) {
 export default async function decorate(block) {
   const link = block.querySelector('a');
   const path = link ? link.getAttribute('href') : block.textContent.trim();
-  const fragment = await loadFragment(path);
-  if (fragment) {
-    const fragmentSection = fragment.querySelector(':scope .section');
-    if (fragmentSection) {
-      block.closest('.section')?.classList.add(...fragmentSection.classList);
-      block.closest('.fragment')?.replaceWith(...fragment.childNodes);
+  if (path && path.startsWith('/')) {
+    if (path.endsWith('.css')) {
+      block.innerText = '';
+      loadCSS(`${path}`);
+    } else if (path.endsWith('.js')) {
+      block.innerText = '';
+      loadScript(`${path}`);
+    } else {
+      const fragment = await loadFragment(path);
+      if (fragment) {
+        const fragmentSection = fragment.querySelector(':scope .section');
+        if (fragmentSection) {
+          block.closest('.section')?.classList.add(...fragmentSection.classList);
+          block.closest('.fragment')?.replaceWith(...fragment.childNodes);
+        }
+      }
     }
   }
 }
