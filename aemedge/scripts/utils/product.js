5deeba49530f7b781b8a1358bafa1d3158246b8e
Joaquin Allende - E44130 <joaquin.allende@cmegroup.com>
2025-12-05 15:31:19 -0300

    Issue 999: new cvol data index block

--- aemedge/scripts/utils/product.js
+++ aemedge/scripts/utils/product.js
@@ -11,6 +11,7 @@ const API_CONFIG = {
   // Expirations endpoint - requires productId parameter
   fullProductWithOptionsEndpoint: '/CmeWS/md/Product/V2/FullProductWithOptions/ProductId/',
   contractsByNumberEndpoint: '/CmeWS/mvc/quotes/v2/contracts-by-number',
+  cvolEndpoint: '/services/cvol',
 };
 
 export function normalizePath(pathname) {
@@ -244,6 +245,46 @@ export async function getContractsByNumber(productId) {
   const response = await apiPost(endpoint, payload, headers);
   const data = getResponseData(response) || response.data;
   return data;
+}
+
+export async function getCvolIndexData(productIds) {
+  const strProductIds = Array.isArray(productIds) ? productIds.join(',') : productIds;
+  const endpoint = `${urlByEnvType()}${API_CONFIG.cvolEndpoint}?symbol=${strProductIds}`;
+
+  try {
+    const response = await apiGet(endpoint, {}, {}, { withCredentials: false });
+    const rawResponse = getResponseData(response) || response.data || [];
+
+    return productIds.map((prodId) => {
+      const item = rawResponse.find(({ symbol }) => prodId === symbol);
+      if (item) {
+        let changeColor = '';
+        if (item.cvolPriceChange && item.cvolPriceChange.length > 0) {
+          if (item.cvolPriceChange[0] === '+') {
+            changeColor = 'positive';
+          } else if (item.cvolPriceChange[0] === '-' && item.cvolPriceChange.length > 1) {
+            changeColor = 'negative';
+          }
+        }
+        return {
+          symbol: item.symbol,
+          cvolPrice: item.cvolPrice,
+          cvolPriceChange: item.cvolPriceChange,
+          insertTime: item.insertTime,
+          changeColor,
+        };
+      }
+      return {
+        symbol: prodId,
+        cvolPrice: '-',
+        cvolPriceChange: '-',
+        insertTime: '-',
+        changeColor: '',
+      };
+    });
+  } catch (e) {
+    return [];
+  }
 }
 
 /**
